364f415883da756d534b10577909dac20524411a
Pau <pauserratosa5@gmail.com>
2021-02-09 19:11:03 +0100

    DB connection configuration.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+//Connect database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("API running"));
 
-app.listen(PORT, () => console.log("Server started on port ${PORT}"));
+app.listen(PORT, () => console.log("Server started on port" + PORT));
